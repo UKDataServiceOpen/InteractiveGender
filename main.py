ba97a5a5b25724633f70b15c5c6a6219ed213e08
Louise Capener <louise.capener@manchester.ac.uk>
2023-05-18 21:55:36 +0100

    Forgot to add file extension on file I was importing...

--- main.py
+++ main.py
@@ -543,10 +543,10 @@ update_highlighted_rows_2(select_religion_2.value)
 
 # Add sex dataset in
 
-so_tot = pd.read_csv('/Users/loucap/Documents/GitWork/InteractiveGender/Data/sex_totals_SO_2')
-so_nr = pd.read_csv('/Users/loucap/Documents/GitWork/InteractiveGender/Data/nr_totals_SO_2')
-gi_tot = pd.read_csv('/Users/loucap/Documents/GitWork/InteractiveGender/Data/sex_totals_GI_2')
-gi_nr = pd.read_csv('/Users/loucap/Documents/GitWork/InteractiveGender/Data/nr_totals_GI_2')
+so_tot = pd.read_csv('sex_totals_SO_2.csv')
+so_nr = pd.read_csv('nr_totals_SO_2.csv')
+gi_tot = pd.read_csv('sex_totals_GI_2.csv')
+gi_nr = pd.read_csv('nr_totals_GI_2.csv')
 
 sourc1 = ColumnDataSource(so_tot)
 
